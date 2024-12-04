2137d8956b7a0d85c2b06961f883e99b66c08a54
vraj patel <vraj.patel@primedemo.in>
2024-12-04 19:06:07 +0530

    added size of fonts

--- src/Components/TextEditor.jsx
+++ src/Components/TextEditor.jsx
@@ -29,6 +29,7 @@ const editorWrapper = useCallback((wrapper) =>{
                 [{ 'script': 'sub' }, { 'script': 'super' }],
                 [{ 'indent': '-1' }, { 'indent': '+1' }],
                 [{ 'direction': 'rtl' }],
+                [{ size: [ 'small', false, 'large', 'huge' ]}]
                 [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
                 [{ 'color': [] }, { 'background': [] }],
                 [{ 'font': [] }],
